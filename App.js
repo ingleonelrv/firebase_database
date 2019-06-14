bd50b41ebefb0dd5576b6830f39ec963b6eff6ab
ingleonelrv <ing.leonelrv@gmail.com>
2019-06-13 20:46:51 -0600

    working: read and write in firebase database real time

--- App.js
+++ App.js
@@ -1,64 +1,84 @@
-import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+import React from "react";
+import { StyleSheet, TextInput, Image, Text, View, Button } from "react-native";
 
-import firebase from 'react-native-firebase';
-
+import firebase from "react-native-firebase";
+let database;
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      fname: "",
+      lname: "",
+      age: "",
+      users: []
+    };
   }
 
   async componentDidMount() {
-    // TODO: You: Do firebase things
-    // const { user } = await firebase.auth().signInAnonymously();
-    // console.warn('User -> ', user.toJSON());
-
-    // await firebase.analytics().logEvent('foo', { bar: '123'});
+    this.initializeDB();
   }
-
+  initializeDB = () => {
+    database = firebase.database();
+  };
+  handleSave = () => {
+    let dataRef = database.ref("users/");
+    let dataPush = dataRef.push(this.state);
+    this.setState({ fname: "", lname: "", age: "" });
+  };
+  handleRead = async () => {
+    let dataRef = database.ref("users/");
+    //update working
+    // dataRef.child("-LhHR3VNwHkQFSfkC699").update({ age: 24 });
+    //To can read without auth() enable from firebase console Auth Anonym
+    dataRef.on("value", snapshot => {
+      let users = [];
+      snapshot.forEach(key => {
+        let user = {
+          id: key.key,
+          user: key._value
+        };
+        users.push(user);
+      });
+      this.setState({ users });
+    });
+  };
   render() {
     return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
-          <Text style={styles.welcome}>
-            Welcome to {'\n'} React Native Firebase
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
-          {Platform.OS === 'ios' ? (
-            <Text style={styles.instructions}>
-              Press Cmd+R to reload,{'\n'}
-              Cmd+D or shake for dev menu
-            </Text>
-          ) : (
-            <Text style={styles.instructions}>
-              Double tap R on your keyboard to reload,{'\n'}
-              Cmd+M or shake for dev menu
-            </Text>
-          )}
-          <View style={styles.modules}>
-            <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
-            {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
-            {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
-            {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
-            {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
-            {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
-            {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
-            {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
-            {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
-            {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
-            {firebase.invites.nativeModuleExists && <Text style={styles.module}>invites()</Text>}
-            {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
-            {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
-            {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
-            {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
-            {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
-          </View>
-        </View>
-      </ScrollView>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="First Name"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={fname => this.setState({ fname })}
+          value={this.state.fname}
+        />
+        <TextInput
+          placeholder="Last Name"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={lname => this.setState({ lname })}
+          value={this.state.lname}
+        />
+        <TextInput
+          placeholder="Age"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={age => this.setState({ age })}
+          value={this.state.age}
+        />
+        <Button style={styles.btn} title="Save" onPress={this.handleSave} />
+        <Button style={styles.btn} title="Read" onPress={this.handleRead} />
+        {this.state.users &&
+          this.state.users.map(user => {
+            return (
+              <View style={styles.userList} key={user.id}>
+                <Text>First Name: {user.user.fname}</Text>
+                <Text>Last Name: {user.user.lname}</Text>
+                <Text>Age: {user.user.age}</Text>
+              </View>
+            );
+          })}
+      </View>
     );
   }
 }
@@ -66,37 +86,26 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
-  logo: {
-    height: 120,
-    marginBottom: 16,
-    marginTop: 64,
-    padding: 10,
-    width: 135,
+  btn: {
+    marginVertical: 15
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  textInput: {
+    height: 40,
+    width: "90%",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginTop: 8
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  modules: {
-    margin: 20,
-  },
-  modulesHeader: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  module: {
-    fontSize: 14,
-    marginTop: 4,
-    textAlign: 'center',
+  userList: {
+    flex: 0,
+    alignItems: "center",
+    width: "100%",
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,.1)"
   }
 });
